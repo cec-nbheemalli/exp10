47a05075dcaef5907b4950f3fde1925073cb33f9
Nitin <nitinsb@cityengineeringcollege.ac.in>
2026-04-08 12:06:07 +0530

    Azure Pipeline

---
+++ Javateam/src/main/java/com/ceconjava/App.java
@@ -0,0 +1,13 @@
+package com.ceconjava;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
